2d52b5fc9df34c008018d3829fb84e10a3a94b35
loriannehutauruk <lori@hutauruk.com>
2019-04-21 20:24:38 -0500

    adding week02piglatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,10 +9,37 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
-
-  // Your code here
-
+  const vowel = "aeiou";
+  let match = -1;
+  let index = -1;
+  // find index of first vowel
+  for (let x = 0; x < word.length && match < 0; x++) {
+    match = vowel.indexOf(word.charAt(x));
+   // console.log(match)
+    if (match >= 0) {
+      index = x;
+     // console.log(index)
+    }
+  }
+  
+  if (index > 0) {
+    word = word.slice(index) + word.substring(0, index) + "ay";
+  } 
+  else {
+    word = word.slice(index) + word.substring(0, 1) + "yay";
+  }
+  return word.toLowerCase();
 }
+console.log(pigLatin("car"));
+console.log(pigLatin("dog"));
+console.log(pigLatin("create"));
+console.log(pigLatin("valley"));
+console.log(pigLatin("egg"));
+console.log(pigLatin("emmission"));
+console.log(pigLatin("HeLlo"));
+console.log(pigLatin("RoCkEt"));
+
+
 
 
 function getPrompt() {
