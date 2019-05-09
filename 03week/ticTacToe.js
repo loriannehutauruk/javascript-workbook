bae71c992242f28a01474d7cd428a4185aac4d5b
loriannehutauruk <lori@hutauruk.com>
2019-05-08 21:04:47 -0500

    adding ticTacToe

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -23,126 +23,42 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-function horizontalWin() {
+function horizontalWin(row) {
+  if (board[0].every(cell => cell === 'X') || board[1].every(cell => cell === 'X') || board[2](cell => cell === 'X'))
+  return true;
 
-  //for loop to check each row
-  for(let i=0; i<=2; i++){
-    let row = board[i];
-
-    //if win in a row returns true
-    if(row[0] == row[1] && row[1] == row[2] && row[0] != ' '){
-      return true;
-    }
-
-  }
-
+return false;
+  
 }
 
 //checks for vertical win
 function verticalWin() {
-
-  //for loop to check each column
-  for(let i=0; i<=2; i++){
-
-    let col = [];
-
-  for(let j=0; j<=2; j++){
-    let x = board[j][i];
-    col.push(x);
-  }
-
-  //if win in a column returns true
-  if(col[0] == col[1] && col[1] == col[2] && col[0] != ' '){
-    return true;
-  }
-
-  }
-
+  if (board[0][0]=== 'X' && board[1][1] === 'X' && board [2][1] === 'X')
+   return true;
+  
 }
 
 //checks for diagonal win
 function diagonalWin() {
-
-  //if one diagonal win returns true
-  if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ' ){
-    return true;
-  }
-  //if other diagonal win returns true
-  else if( board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' '){
-    return true;
-  }
-
+  if (board[0][0]=== 'X' && board[1][1] === 'X' && board[2][2] === 'X')
+   return true;
 }
 
 //checks if there is a winner with player marker as parameter
 function checkForWin(player) {
-
-  //calls each check function
-  horizontalWin();
-  verticalWin();
-  diagonalWin();
-
-  //if any check function returns true declares winner
-  if (horizontalWin() === true || verticalWin() === true || diagonalWin() === true){
-
-    //prints winning board
-    printBoard();
-
-    //resets board after a win
-    console.log(player + "'s win!");
-    board = [
-      [' ', ' ', ' '],
-      [' ', ' ', ' '],
-      [' ', ' ', ' ']
-    ];
-    return true;
-
-  }  
-    
+  return true;
 }
-
-//function to print markers
+//function to print markers and switch turns
 function ticTacToe(row, column) {
-
-  //converts input to number
-  row = Number(row);
-  column = Number(column);
-
-  //possible choies array declaration
-  let choices = [0, 1, 2];
-
-  //if number is not one of the possible choices print 'input out of range'
-  if(choices.includes(row) && choices.includes(column)){
-
-    if(board[row][column] === ' '){
-
-      //takes input and prints current marker in correct row and column
-      board[row][column] = playerTurn;
-
-      //checks for winner after every turn
-      checkForWin(playerTurn);
-
-      //alternates players
-      if(playerTurn == 'X'){
-        playerTurn = 'O';
-      }
-      else{
-        playerTurn = 'X';
-      }
-    }
-    else{
-      console.log("That space is occupied");
-      return "That space is occupied";
-    }
-
+  board[row][colum] = playerTurn;
+  if (playerTurn = 'X') {
+    playerTurn = 'O';
   }
-  
-  else{
-    console.log("Please enter a valid input.");
-    return "Please enter a valid input.";
+  else {
+    playerTurn = 'X';
   }
+
 }
-
 
 function getPrompt() {
   printBoard();
