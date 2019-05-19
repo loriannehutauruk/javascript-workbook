783d803f2848ba185aa76fcb4f2ef77c93254d0b
loriannehutauruk <lori@hutauruk.com>
2019-05-19 11:24:51 -0500

    adding towers of hanoi

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,24 +19,42 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+function movePiece (startStack, endStack) {
+  // Your code here. use method that pops the last item of an array and pushes it to another place. 
+let elmt = stacks[startStack].pop();
+stacks[endStack].push(elmt);
 }
 
-function isLegal() {
+function isLegal (startStack, endStack) {
+  //can only push if the existing value in the array is greater than the value coming in or if the array is empty. first check is empty.
+  if (stacks[startStack].length == 0 && stacks[endStack].length == 0) {
+    return false;
+  }
+  let elmt1 = stacks[startStack][stacks[startStack].length-1];
+
+  if (elmt1 > 0 && stacks[endStack].length == 0){
+    return true;
+  }
+
+  let elmt2 = stacks[endStack][stacks[endStack].length-1];
+  if (elmt2 > elmt1) 
+    return true;
+  return false;
+  }
+  
+
+function checkForWin (stacks) {
   // Your code here
 
+if (stacks.b.includes(4,0) && stacks.b.includes(3,1) && stacks.b.includes(2,2) && stacks.b.includes(1,3)) {
+  return true;
 }
-
-function checkForWin() {
-  // Your code here
 
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+movePiece (startStack, endStack)
 }
 
 function getPrompt() {
