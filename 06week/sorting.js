6b7ca35cc5c7c76dd0bfba161e43127e16ead23f
loriannehutauruk <lori@hutauruk.com>
2019-06-10 20:35:34 -0500

    adding sorting with modified code

--- 06week/sorting.js
+++ 06week/sorting.js
@@ -14,10 +14,10 @@ const sumEvens= num.reduce (
 );
 console.log(sumEvens);
 
-// Find the index of the first value when added to it's index = 512 (#ATX!!)
-const atxIdx;
+// Find the index of the first value when  its accumulated value is euql to 512 (#ATX!!)
+// const atxIdx;
 
-console.log(`index: ${atxIdx}, value: ${nums[atxIdx]}`);
+// console.log(`index: ${atxIdx}, value: ${nums[atxIdx]}`);
 
 const weather = [
   { id: 5743823523151872,
@@ -87,10 +87,17 @@ const weather = [
 ],
 
 //using a higher order function, create an array of the unique 'weather_state_name' values of the weather array. Your function should return the following array ['Light Cloud', 'Heavy Cloud', 'Showers']
-const weatherStates = 
-console.log(weatherStates)
+
+weatherStates = weather.map(x => x.weather_state_name);
+console.log(weatherStates);
 
 //find the id of the object in weather that has a min_temp of 15.915
 
-const idealTemp =
+idealTemp = weather.filter((weatherState, i) => weatherState.min_temp < 15.915, console.log(weather.id))
 console.log(idealTemp)
+
+
+//How can I log the id value after i've filtered through with the temp value
+// idealTemp = weather.filter(x => x.min_temp < 15.915)
+// console.log(idealTemp)
+
